ef72ddc4c13ce3f65d28a32702ef6137378bc80f
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 09:57:51 +0200

    done

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-# your function goes here
+# your function sum_of_numbers goes here
+def sum_of_numbers(list):
+    return sum(list)
 
 if __name__ == "__main__":
     list = [1, 2, 3, 4, 5]
+    print(sum_of_numbers(list))
     # and here
--- main_test.py
+++ main_test.py
@@ -1,13 +1,13 @@
 import main
-def test_sum_positive_numbers(self):
+def test_sum_positive_numbers():
     """Testfall für eine Liste von positiven Zahlen."""
     assert(main.sum_of_numbers([1, 2, 3, 4, 5]), 15)
 
-def test_sum_mixed_numbers(self):
+def test_sum_mixed_numbers():
     """Testfall für eine Liste von gemischten Zahlen (positiv und negativ)."""
     assert(main.sum_of_numbers([1, -1, 2, -2]), 0)
 
-def test_sum_single_number(self):
+def test_sum_single_number():
     """Testfall für eine Liste mit einer einzigen Zahl."""
     assert(main.sum_of_numbers([7]), 7)
 
