a140f872ab3e0aaf0e3bec75b3c37f3250b5abf6
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 09:56:23 +0200

    done

--- main_test.py
+++ main_test.py
@@ -1,16 +1,16 @@
 import main
 def test_sum_positive_numbers(self):
     """Testfall für eine Liste von positiven Zahlen."""
-    self.assertEqual(main.sum_of_numbers([1, 2, 3, 4, 5]), 15)
+    assert(main.sum_of_numbers([1, 2, 3, 4, 5]), 15)
 
 def test_sum_mixed_numbers(self):
     """Testfall für eine Liste von gemischten Zahlen (positiv und negativ)."""
-    self.assertEqual(main.sum_of_numbers([1, -1, 2, -2]), 0)
+    assert(main.sum_of_numbers([1, -1, 2, -2]), 0)
 
 def test_sum_single_number(self):
     """Testfall für eine Liste mit einer einzigen Zahl."""
-    self.assertEqual(main.sum_of_numbers([7]), 7)
+    assert(main.sum_of_numbers([7]), 7)
 
 def test_sum_empty_list(self):
     """Testfall für eine leere Liste."""
-    self.assertEqual(main.sum_of_numbers([]), 0)
+    assert(main.sum_of_numbers([]), 0)
