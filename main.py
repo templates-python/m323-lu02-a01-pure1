5009ff5e392baf190256bf1406049d27b4b9cd6f
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:02:28 +0200

    Ran pyink on branch solution

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 def sum_of_numbers(list):
     return sum(list)
 
+
 if __name__ == "__main__":
     list = [1, 2, 3, 4, 5]
     print(sum_of_numbers(list))
--- main_test.py
+++ main_test.py
@@ -1,16 +1,21 @@
 import main
+
+
 def test_sum_positive_numbers():
     """Testfall für eine Liste von positiven Zahlen."""
-    assert(main.sum_of_numbers([1, 2, 3, 4, 5]), 15)
+    assert (main.sum_of_numbers([1, 2, 3, 4, 5]), 15)
+
 
 def test_sum_mixed_numbers():
     """Testfall für eine Liste von gemischten Zahlen (positiv und negativ)."""
-    assert(main.sum_of_numbers([1, -1, 2, -2]), 0)
+    assert (main.sum_of_numbers([1, -1, 2, -2]), 0)
+
 
 def test_sum_single_number():
     """Testfall für eine Liste mit einer einzigen Zahl."""
-    assert(main.sum_of_numbers([7]), 7)
+    assert (main.sum_of_numbers([7]), 7)
+
 
 def test_sum_empty_list():
     """Testfall für eine leere Liste."""
-    assert(main.sum_of_numbers([]), 0)
+    assert (main.sum_of_numbers([]), 0)
